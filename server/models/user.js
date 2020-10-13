287171bd143d63c9c9b9ed824a2f18a42b640eb4
chas <hugginsc10@gmail.com>
2020-10-13 17:52:11 -0400

    deploy

--- server/models/user.js
+++ server/models/user.js
@@ -8,5 +8,4 @@ const userSChema = mongoose.Schema({
         match:  /[a-z0–9!#$%&’*+/=?^_`{|}~-]+(?:\.[a-z0–9!#$%&’*+/=?^_`{|}~-]+)*@(?:[a-z0–9](?:[a-z0–9-]*[a-z0–9])?\.)+[a-z0–9](?:[a-z0–9-]*[a-z0–9])?/,
     }
 });
-
 module.exports = mongoose.model("User", userSchema);
